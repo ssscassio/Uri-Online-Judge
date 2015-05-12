0b57d3dd81427f43bdcfae5c97534893dd2c2db1
Cassio Silva <sss.cassio@gmail.com>
2015-05-12 13:16:29 -0300

    Problema 1050

---
+++ 1050 DDD/1050.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int ddd;
+    cin >> ddd;
+
+    if (ddd ==61)
+        cout << "Brasilia" << endl;
+    else if (ddd ==71)
+        cout << "Salvador" << endl;
+    else if (ddd ==11)
+        cout << "Sao Paulo" << endl;
+    else if (ddd ==21)
+        cout << "Rio de Janeiro" << endl;
+    else if (ddd ==32)
+        cout << "Juiz de Fora" << endl;
+    else if (ddd ==19)
+        cout << "Campinas" << endl;
+    else if (ddd ==27)
+        cout << "Vitoria" << endl;
+    else if (ddd ==31)
+        cout << "Belo Horizonte" << endl;
+    else
+        cout << "DDD nao cadastrado" << endl;
+
+return 0;
+}
