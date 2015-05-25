5c61c38dafe60af01bfeef15fd0185362f8872e2
Cassio Silva <sss.cassio@gmail.com>
2015-05-25 08:23:25 -0300

    Probema 1134

---
+++ 1134 Tipo de Combustível/1134.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int alcool, gasolina, diesel;
+    alcool = gasolina = diesel = 0;
+    int resp;
+
+    cin >> resp;
+    while (resp != 4){
+
+        switch (resp){
+            case 1:
+                alcool++;
+                break;
+            case 2:
+                gasolina++;
+                break;
+            case 3:
+                diesel++;
+                break;
+            default:
+                break;
+        }
+
+        cin >> resp;
+    }
+
+    cout << "MUITO OBRIGADO" << endl;
+    cout << "Alcool: " << alcool << endl;
+    cout << "Gasolina: " << gasolina << endl;
+    cout << "Diesel: " << diesel << endl;
+    return 0;
+}
