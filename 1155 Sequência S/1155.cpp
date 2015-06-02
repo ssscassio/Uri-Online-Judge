199a35346bb304b610e055f09e40a288a4bada29
Cassio Silva <sss.cassio@gmail.com>
2015-06-02 01:58:15 -0300

    Problema 1155

---
+++ 1155 Sequência S/1155.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+	 
+using namespace std;
+ 
+int main() {
+ 
+	float S;
+	S = 0;
+	for(int i = 1; i<=100; i++)
+		S+= (float) 1/i;
+	
+
+	printf("%.2f\n", S);
+	 
+    return 0;
+}
