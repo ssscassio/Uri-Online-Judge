bb895ed3595d149d4501deb01f3f202127bfadda
Cassio Silva <sss.cassio@gmail.com>
2015-07-26 15:33:57 -0300

    Problema 1159

--- 1159 Soma de Pares Consecutivos/1159.cpp
+++ 1159 Soma de Pares Consecutivos/1159.cpp
@@ -8,13 +8,12 @@ int main() {
 
 	while (cin>>X && X!=0){
 	soma = 0;
-		if(X%2 ==1)
+		if(X%2 ==1 || X%2 == -1)
 			X++;
-		for (int i = 0; i <= 4; i++, X += 2)
+		for (int i = 0; i < 5; i++, X += 2)
 			soma+=X;		
 		cout << soma << endl; 
 
 	}
  
-    return 0;
 }
